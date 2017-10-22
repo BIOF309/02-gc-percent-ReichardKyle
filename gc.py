522dcd3d9e2274db9e193c4da4637cd5f33abeb1
ReichardKyle <31736864+ReichardKyle@users.noreply.github.com>
2017-10-22 13:52:21 -0400

    commit

---
+++ gc.py
@@ -0,0 +1,13 @@
+sequence = input("What is your sequence?")
+
+
+#count number of guanine bases
+num_g = sequence.count(G)
+#Count number of cysteine bases
+num_c = sequence.count(C)
+#count total number of bases
+length=len(sequence)
+#calculate percent GC
+gc_percent = (num_g + num_c)/length * 100
+#print % GC
+print("Your GC % is " +str(gc_percent) + "% :D")
