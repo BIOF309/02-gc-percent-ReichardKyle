ca5b2972964be5c81fd0a791292f365f336e48cf
ReichardKyle <31736864+ReichardKyle@users.noreply.github.com>
2017-10-30 18:02:53 -0400

    finished assignment

--- gc.py
+++ gc.py
@@ -11,3 +11,17 @@ length=len(sequence)
 gc_percent = (num_g + num_c)/length * 100
 #print % GC
 print("Your GC % is " +str(gc_percent) + "% :D")
+sequence = input(What is your sequence?)
+
+
+#count number of guanine bases
+num_g = sequence.count(G)
+#Count number of cysteine bases
+num_c = sequence.count(C)
+#count total number of bases
+length=len(sequence)
+#calculate percent GC
+gc_percent = (num_g + num_c)/length * 100
+#print % GC
+print(Your GC % is  +str(gc_percent) + % :D)
+
